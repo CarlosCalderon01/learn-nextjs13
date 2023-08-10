69cafe69f6b5a65197994994f5d602fca8c16496
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-08-09 21:34:48 -0500

    upgrade npm dependency and new get fetch
    i have a problems with dependency and i need learned manage npm and i have a new control on dependency

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,22 @@
-// `app/page.tsx` is the UI for the `/` URL
-export default function Page() {
-  return <h1>Hello, Home page home!</h1>
+//Example: react server component
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
 }
+
+async function homePage() {
+  const users = await fetchUsers();
+  return (
+    <div>
+      <div>
+        <h1>Imprimiendo json</h1>
+      </div>
+      <div>
+        <p>{JSON.stringify(users)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default homePage;
